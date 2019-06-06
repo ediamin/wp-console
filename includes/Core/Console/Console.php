ed9d5c88d4be99df28bc84aeda5e79bb889894f2
Edi Amin <to.ediamin@gmail.com>
2019-06-06 14:42:53 +0600

    Add enqueue script hook

--- includes/Core/Console/Console.php
+++ includes/Core/Console/Console.php
@@ -14,6 +14,12 @@ class Console {
     public function __construct() {
         add_action( 'wp_console_controllers', [ $this, 'add_controller' ] );
         add_action( 'wp_console_rest_controllers', [ $this, 'add_rest_controller' ] );
+
+        if ( is_admin() ) {
+            add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+        } else {
+            add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+        }
     }
 
     /**
@@ -42,5 +48,27 @@ class Console {
     public function add_rest_controller( $controllers ) {
         $controllers['console'] = new RestController();
         return $controllers;
+    }
+
+    /**
+     * Enqueue console scripts
+     *
+     * @since WP_CONSOLE_SINCE
+     *
+     * @return void
+     */
+    public function enqueue_scripts() {
+        wp_enqueue_style( 'wp-console', WP_CONSOLE_ASSETS . '/css/wp-console.css', [], WP_CONSOLE_VERSION );
+        wp_enqueue_script( 'wp-console', WP_CONSOLE_ASSETS . '/js/wp-console.js', [ 'jquery', 'wp-api-fetch' ], WP_CONSOLE_VERSION, true );
+
+        $data = [
+            'rest' => [
+                'root'      => untrailingslashit( get_rest_url() ),
+                'nonce'     => wp_create_nonce( 'wp_rest' ),
+                'namespace' => 'wp-console/v1',
+            ],
+        ];
+
+        wp_localize_script( 'wp-console', 'WPConsole', $data );
     }
 }
