00352780bb8f2d04ab5fecbfe5d1221a2c624553
Edi Amin <to.ediamin@gmail.com>
2019-06-21 07:49:47 +0600

    Add npm zip command

---
+++ build/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dest=build/wp-console
+
+rm -rf $dest
+mkdir $dest
+cp -r assets $dest
+cp -r includes $dest
+cp -r languages $dest
+cp -r vendor $dest
+cp README.md $dest
+cp composer.json $dest
+cp index.php $dest
+cp license.txt $dest
+cp readme.txt $dest
+cp wp-console.php $dest
+
+cd build
+zip wp-console.zip wp-console -r
