9631487eabb447bae728e0fb1af48270bb49d935
Edi Amin <to.ediamin@gmail.com>
2022-07-25 21:52:23 +0600

    Update the list of unnecessary directories and files from zip

--- build/index.js
+++ build/index.js
@@ -56,7 +56,57 @@ include.forEach( ( item ) => {
     shell.cp( '-r', resolve( '../', item ), resolve( DEST, item ) );
 } );
 
-shell.rm( '-rf', resolve( DEST, 'vendor/psy/psysh/test' ) );
+// Remove unnecessary directories and files.
+[
+    'vendor/jakub-onderka/php-console-color/.gitignore',
+    'vendor/jakub-onderka/php-console-color/.travis.yml',
+    'vendor/jakub-onderka/php-console-color/build.xml',
+    'vendor/jakub-onderka/php-console-color/example.php',
+    'vendor/jakub-onderka/php-console-color/phpunit.xml',
+    'vendor/jakub-onderka/php-console-color/tests',
+    'vendor/jakub-onderka/php-console-highlighter/.gitignore',
+    'vendor/jakub-onderka/php-console-highlighter/.travis.yml',
+    'vendor/jakub-onderka/php-console-highlighter/build.xml',
+    'vendor/jakub-onderka/php-console-highlighter/examples',
+    'vendor/jakub-onderka/php-console-highlighter/phpunit.xml',
+    'vendor/jakub-onderka/php-console-highlighter/tests',
+    'vendor/nikic/php-parser/.gitignore',
+    'vendor/nikic/php-parser/.travis.yml',
+    'vendor/nikic/php-parser/CHANGELOG.md',
+    'vendor/nikic/php-parser/UPGRADE-1.0.md',
+    'vendor/nikic/php-parser/UPGRADE-2.0.md',
+    'vendor/nikic/php-parser/UPGRADE-3.0.md',
+    'vendor/nikic/php-parser/doc',
+    'vendor/nikic/php-parser/phpunit.xml.dist',
+    'vendor/nikic/php-parser/test',
+    'vendor/nikic/php-parser/test_old',
+    'vendor/psr/log/.gitignore',
+    'vendor/psr/log/Psr/Log/Test',
+    'vendor/psy/psysh/.editorconfig',
+    'vendor/psy/psysh/.github',
+    'vendor/psy/psysh/.gitignore',
+    'vendor/psy/psysh/.phan',
+    'vendor/psy/psysh/.php_cs',
+    'vendor/psy/psysh/.styleci.yml',
+    'vendor/psy/psysh/.travis.yml',
+    'vendor/psy/psysh/phpunit.xml.dist',
+    'vendor/psy/psysh/test',
+    'vendor/symfony/console/.gitignore',
+    'vendor/symfony/console/Tester',
+    'vendor/symfony/console/Tests',
+    'vendor/symfony/console/phpunit.xml.dist',
+    'vendor/symfony/debug/.gitignore',
+    'vendor/symfony/debug/CHANGELOG.md',
+    'vendor/symfony/debug/Tests',
+    'vendor/symfony/debug/phpunit.xml.dist',
+    'vendor/symfony/var-dumper/.gitignore',
+    'vendor/symfony/var-dumper/CHANGELOG.md',
+    'vendor/symfony/var-dumper/Test',
+    'vendor/symfony/var-dumper/Tests',
+    'vendor/symfony/var-dumper/phpunit.xml.dist',
+].forEach( ( deletePath ) => {
+    shell.rm( '-rf', resolve( DEST, deletePath ) );
+} );
 
 console.log( 'Making zip...' );
 shell.exec( `cd ${ resolve() } && zip ${ zip } wp-console -rq` );
