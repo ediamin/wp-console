6bbdfc40f6800464aaeae1b999f96141f12cc0b2
Edi Amin <to.ediamin@gmail.com>
2023-11-25 21:06:30 +0600

    Refactor support for PHP version 7.4

---
+++ lib/overrides.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Common overrides for all PHP versions.
+ */
+
+require_once __DIR__ . '/overrides/psy/psysh/src/Output/ShellOutput.php';
+require_once __DIR__ . '/overrides/psy/psysh/src/Output/Theme.php';
+require_once __DIR__ . '/overrides/psy/psysh/src/Shell.php';
